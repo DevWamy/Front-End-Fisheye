9cede764f53ecb6cd5ce6b3204cf8eaa1993b05e
Wamy Wam <nadia.bolivant@gmail.com>
2022-03-21 19:17:37 +0100

    Pictures added

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,9 @@
 //Mettre le code JavaScript lié à la page photographer.html
+const photographersData = async() => {
+    const data = await fetch('./data/photographers.json');
+    const mediaData = await data.json();
+    const media = mediaData.media;
+    return {photographersData: [...media]}
+}
+
+photographersData();
